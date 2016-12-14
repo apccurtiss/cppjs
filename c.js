46b44ec5f200cad9c484d201780533a45dd9c9fc
Alexander Curtiss <apccurtiss@gmail.com>
2016-12-13 17:03:29 -0700

    Added basic parsing and evaluation - it can do basic math now!

--- c.js
+++ c.js
@@ -2,107 +2,227 @@ function preprocess(code) {
   return code;
 }
 
-// please God tell me there's a better way to do this
-
-// symbols
-var OParen = function() {};
-OParen.pattern = /^\(/;
-var CParen = function() {};
-CParen.pattern = /^\)/;
-var Semi = function() {};
-Semi.pattern = /^;/
-
-// names
-var Ident = function(v) {
-  this.value = v;
+var Symbol = function(type, pattern) {
+  this.type = type;
+  this.pattern = pattern;
 }
-Ident.pattern = /^[a-zA-Z]+/;
 
-// variables
-var Str = function(v) {
-  this.value = v;
+var symbols = [
+  new Symbol("OParen", /^\(/),
+  new Symbol("CParen", /^\)/),
+  new Symbol("Plus", /^\+/),
+  new Symbol("Minus", /^\-/),
+  new Symbol("Mul", /^\*/),
+  new Symbol("Div", /^\//),
+  new Symbol("Semi", /^;/),
+  new Symbol("Cmp", /^==/),
+  new Symbol("Set", /^=/),
+  // names
+  new Symbol("Ident", /^[-a-zA-Z_][-a-zA-Z0-9_]+/),
+  // variables
+  new Symbol("Int", /^[0-9]+/),
+  // whitespace
+  new Symbol("Whitespace", /^\s+/),
+]
+
+function parse_token(code) {
+  var m;
+  for(var i = 0; i < symbols.length; i++) {
+    if(m = code.match(symbols[i].pattern)) {
+      return new Token(symbols[i].type, m[0]);
+    }
+  }
+  throw "Unknown pattern: " + code;
 }
-Str.pattern = /^".*"/;
 
-// whitespace
-var Whitespace = function() {};
-Whitespace.pattern = /^\s/;
+var Token = function(type, string) {
+  this.type = type;
+  this.string = string;
+}
 
 function tokenize(code) {
-  var patterns = [
-    OParen,
-    CParen,
-    Semi,
-    Ident,
-    Str,
-    Whitespace,
-  ]
-
   var tokens = [];
-
-  var m;
   do {
-    for(i in patterns) {
-      m = code.match(patterns[i].pattern);
-      if(m) {
-        if(patterns[i] !== Whitespace) {
-          tokens.push(new patterns[i](m[0]));
-        }
-        code = code.substring(m[0].length);
-        break;
-      }
+    var token = parse_token(code);
+    if(token.type != "Whitespace") {
+      tokens.push(token);
     }
-  } while (m);
+    code = code.substring(token.string.length);
+  } while (code.length);
 
   return tokens;
 }
 
-Call = function(name, args) {
-  this.name = name;
-  this.args = args;
-}
 
-function treeify(tokens) {
-  if(tokens[0] instanceof Ident) {
-    if(tokens[1] instanceof OParen) {
-      args = [];
-      var i = 2;
-      while(!(tokens[i] instanceof CParen)) {
-        if(tokens[i] === undefined) {
-          console.log("Unexpected end of file");
-        }
-        args.push(tokens[i]);
-        i++;
-      }
-      return new Call(tokens[0].value, args);
+var types = ["int", "float"];
+is_type = function(ident) {
+  for(i in types) {
+    if(ident == types[i]) {
+      return true;
     }
   }
-  else {
-    console.log("Unexpected token " + tokens[0]);
+  return false;
+}
+
+Bop = function(bop, e1, e2) {
+  this.bop = bop;
+  this.e1 = e1;
+  this.e2 = e2;
+}
+
+Int = function(value) {
+  this.value = value;
+}
+
+require = function(cond) {
+  if(!cond) throw "Requirement failed";
+}
+
+// TODO(alex) use generator instead of array of tokens?
+function treeify(tokens) {
+  function expect(type) {
+    if(!tokens.length) {
+      throw "Unexpected end of file";
+    }
+    if(type != tokens[0].type) {
+      throw "Expected " + type + " but got " + tokens[0].type;
+    }
   }
+  function require(type) {
+    expect(type);
+    token = tokens[0];
+    tokens = tokens.slice(1);
+    return token;
+  }
+  function pop(type) {
+    try {
+      return require(type)
+    }
+    catch(err) {
+      return false;
+    }
+  }
+  function plus() {
+    function plusses(acc) {
+      if(pop("Plus")) {
+        return plusses(new Bop("Plus", acc, mult()));
+      }
+      else if(pop("Minus")) {
+        return plusses(new Bop("Minus", acc, mult()));
+      }
+      else {
+        return acc;
+      }
+    }
+    return plusses(mult());
+  }
+  function mult() {
+    function mults(acc) {
+      if(pop("Mul")) {
+        return mults(new Bop("Mul", acc, atom()));
+      }
+      else if(pop("Div")) {
+        return mults(new Bop("Div", acc, atom()));
+      }
+      else {
+        return acc;
+      }
+    }
+    return mults(atom());
+  }
+  function atom() {
+    var a;
+    if(a = pop("Int")) {
+      return new Int(parseInt(a.string));
+    }
+    else if(pop("OParen")) {
+      var p = plus();
+      require("CParen");
+      return p;
+    }
+    else {
+      throw "Unexpected token: " + tokens[0].type + " (" + tokens[0].string + ")";
+    }
+  }
+  return plus(tokens);
 }
 
 function typecheck(ast, memory) {
-
+  return true;
 }
 
-function run(ast, memory) {
-  if(ast instanceof Call) {
-    memory[ast.name](ast.args);
+function is_value(ast) {
+  if(ast instanceof Int) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
-memory = {
+function step(ast) {
+  if(ast instanceof Bop) {
+    if(!is_value(ast.e1)) {
+      return new Bop(ast.bop, step(ast.e1), ast.e2);
+    }
+    else if(!is_value(ast.e2)) {
+      return new Bop(ast.bop, ast.e1, step(ast.e2));
+    }
+    else {
+      if(ast.bop == "Plus") {
+        return new Int(ast.e1.value + ast.e2.value);
+      }
+      if(ast.bop == "Minus") {
+        return new Int(ast.e1.value - ast.e2.value);
+      }
+      if(ast.bop == "Mul") {
+        return new Int(ast.e1.value * ast.e2.value);
+      }
+      if(ast.bop == "Div") {
+        return new Int(ast.e1.value / ast.e2.value);
+      }
+    }
+  }
+  else {
+    return ast;
+  }
+}
+
+function run(ast, memory) {
+  var steps = [ast];
+  var stepno = 0;
+  do {
+    steps[stepno + 1] = step(steps[stepno]);
+    stepno++;
+    console.log(steps[stepno]);
+  }
+  while(!is_value(steps[stepno]));
+
+}
+
+var memory = {
+  // code
   print: (arg) => { console.log(arg[0].value) },
+  // globals
+  globals: {},
+  // stack
+  stack: [],
+  // heap
+  heap: [],
 }
 
 function parse(code) {
   var pp = preprocess(code);
   var tokens = tokenize(code);
+  console.log("Tokens:")
+  console.log(tokens);
   var ast = treeify(tokens);
+  console.log("AST:")
+  console.log(ast);
   typecheck(ast, memory);
+  console.log("Output:")
   run(ast, memory);
 }
 
-code = "print(\"Hello world!\");"
+code = "((1 + 2) * 3 / 9 + 17) / 6";
 parse(code);
